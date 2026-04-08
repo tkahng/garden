2037ea108719fdb4330931e0bdb29f25dafd7af4
tkahng <tkahng@gmail.com>
2026-04-08 13:44:22 -0700

    feat: tag auth and webhook controllers, mark public endpoints

--- src/main/java/io/k2dv/garden/payment/controller/WebhookController.java
+++ src/main/java/io/k2dv/garden/payment/controller/WebhookController.java
@@ -2,6 +2,8 @@ package io.k2dv.garden.payment.controller;
 
 import io.k2dv.garden.payment.config.StripeProperties;
 import io.k2dv.garden.payment.service.PaymentService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/webhooks")
 @RequiredArgsConstructor
+@Tag(name = "Payments", description = "Checkout and payment webhook endpoints")
+@SecurityRequirements({})
 public class WebhookController {
 
     private final PaymentService paymentService;
